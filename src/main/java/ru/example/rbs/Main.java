cb3021fd3e4199a955c5aefee631e4d1227b0b77
ivanov-ev <162356152+ivanov-ev@users.noreply.github.com>
2024-03-06 20:34:24 +0300

    Update Main.java: added msg

--- src/main/java/ru/example/rbs/Main.java
+++ src/main/java/ru/example/rbs/Main.java
@@ -1,7 +1,10 @@
 package ru.example.rbs;
 
 public class Main {
+
+    private static String msg = "Hello QA GURU!!!!!!!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello QA GURU!!!!!!!!!!!");
+        System.out.println(msg);
     }
 }
