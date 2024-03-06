08e7bb020bb9559ac0d3fe375f1ee6487376225d
Eugene Ivanov <email.eugene-ivanov@yandex.ru>
2024-03-06 20:36:20 +0300

    Updated Main.java in IDEA

--- src/main/java/ru/example/rbs/Main.java
+++ src/main/java/ru/example/rbs/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA GURU!");
     }
+
+    private static String getMessage () {
+        return "Hello QA GURU!";
+    }
 }
